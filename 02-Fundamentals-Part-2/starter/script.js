ce9231bd091bf6a8e2b2c5e1a8150d6df15c6490
danielwalbridge <danielwalbridge@gmail.com>
2022-12-22 16:24:17 -0500

    Functions lecture code

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -11,3 +11,21 @@ if (hasDriversLicense) console.log(" I can drive")
  // strict mode will not allow us to use reserved words for variables
  const interface =' butt'
  */
+
+ /*
+  function logger() {
+     console.log('my cat Delphi is very thicc')
+ }
+
+ logger();
+ logger();
+
+ function fruitProcessor(amountOfApples, amountOfOranges) {
+     console.log(amountOfApples, amountOfOranges)
+     const juice = `juice with ${amountOfApples} apples and ${amountOfOranges} oranges.`
+     return juice
+ }
+
+ fruitProcessor(6,19);
+  */
+
