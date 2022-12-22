f864b6692ed376ccb8b8be128745a5b31a494139
danielwalbridge <danielwalbridge@gmail.com>
2022-12-22 16:10:33 -0500

    strict mode lecture code

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,13 @@
+ 'use strict';
+
+
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log(" I can drive")
+
+ // strict mode will not allow us to use reserved words for variables
+ const interface =' butt'
+ */
