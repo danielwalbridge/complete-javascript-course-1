3ebc800ef711277e37c698f11942b02df1df080e
danielwalbridge <danielwalbridge@gmail.com>
2023-01-18 17:53:11 -0500

    functions calling functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -39,3 +39,26 @@
 // transformer('JavaScript is the best', upperFirstWord)
 // // using oneWord as the call back function in transformer
 // transformer("JavaScript is the best", oneWord)
+
+
+
+// Functions returning functions
+//
+// const greet = function(greeting) {
+//   return function(name) {
+//     console.log(`${greeting} ${name}`);
+//   }
+// }
+//
+// // saving the first function as variable.
+// const greeterYo = greet('Yo');
+// // passes in the name.
+// greeterYo('Lil B');
+// greeterYo('Delphi');
+//
+// // can do it in one go.
+// greet('Hello')('Mr.Boner Jam 2000')
+//
+// const greetA = greeting => name =>   console.log(`${greeting} ${name}`);
+//
+// greetA('Hi')('BootyClap')
