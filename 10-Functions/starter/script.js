21091040df65ded09acc85fcbad87720c0cecbb5
danielwalbridge <danielwalbridge@gmail.com>
2023-01-18 17:13:47 -0500

    defult parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+// const bookingArr = [];
+//
+// const createBooking = function(flightNumber, numPass = 1, price = 0) {
+//
+//   const booking = {
+//     flightNumber,
+//     numPass,
+//     price
+//   }
+//   console.log(booking);
+//   bookingArr.push(booking);
+// }
+//
+// createBooking('322222', 50, 100.00);
+// createBooking('33313');
