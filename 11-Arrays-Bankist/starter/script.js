f0a0db8e36f6f53bbe0b76f5d131603f25a1dba1
danielwalbridge <danielwalbridge@gmail.com>
2023-01-19 17:48:49 -0500

    at method of the array

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -102,3 +102,9 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 //
 // // join
 // console.log(letters.join(' MEOW '));
+
+//
+// const arr = [23,44,11];
+// console.log(arr[0]);
+// console.log(arr.at(0));
+// console.log(arr.at(-1));
