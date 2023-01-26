c9a2c60c37324a0738b7ede82ff1a969c9a4efb7
danielwalbridge <danielwalbridge@gmail.com>
2023-01-26 18:52:12 -0500

    l4

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -337,3 +337,13 @@ btnSort.addEventListener('click', function (e) {
 // });
 
 
+// Numeric Separators
+// 287,460,000,000
+// const diameter = 287_460_000_000;
+// console.log(diameter);
+//
+// const transferFee = 15_00;
+// console.log(transferFee);
+//
+// console.log(Number('23_22'
+// ));
